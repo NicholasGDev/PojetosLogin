127a7dac73540784441e317173517907c3a2dc87
NicholasGDev <nicholas.goulart1@gmail.com>
2019-08-06 17:23:40 -0300

    Aula 06/08/2018

---
+++ app/src/main/java/br/sp/senac/pojetoslogin/ForgotPwdActivity.java
@@ -0,0 +1,14 @@
+package br.sp.senac.pojetoslogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ForgotPwdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.forgot_pwd);
+    }
+}
---
+++ app/src/main/java/br/sp/senac/pojetoslogin/LoginDiferentActivity.java
@@ -0,0 +1,14 @@
+package br.sp.senac.pojetoslogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginDiferentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_diferent_layout);
+    }
+}
